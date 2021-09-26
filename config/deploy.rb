809e760171481412680caf8fcd5a5bfecab88457
Simao Belchior <simaobelchior@gmail.com>
2021-09-26 22:05:13 +0100

    Upgrade Rails and Ruby. Fix index

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.12.0"
+lock "~> 3.16.0"
 
 set :application, "simaobelchior"
 set :repo_url, "git@github.com:simaob/simaobelchior.git"
 
-set :rvm_ruby_version, '2.6.3'
+set :rvm_ruby_version, '2.7.4'
 set :user, 'simaob'
 
 set :linked_files, %w{config/master.key}
