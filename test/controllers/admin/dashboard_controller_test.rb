970e67e6297664c197bf5e17b58a67c760a6111d
Simão Belchior <simaobelchior@gmail.com>
2025-10-08 14:47:10 +0100

    Fix fixtures

--- test/controllers/admin/dashboard_controller_test.rb
+++ test/controllers/admin/dashboard_controller_test.rb
@@ -1,8 +1,16 @@
 require "test_helper"
 
 class Admin::DashboardControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get admin_dashboard_index_url
+  test "should redirect to login when not authenticated" do
+    get admin_root_url
+    assert_redirected_to login_path
+  end
+
+  test "should get index when authenticated" do
+    # Login first
+    post login_path, params: { email: "user1@example.com", password: "password" }
+
+    get admin_root_url
     assert_response :success
   end
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,18 +1,31 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get sessions_new_url
+  test "should get login page" do
+    get login_path
     assert_response :success
   end
 
-  test "should get create" do
-    get sessions_create_url
-    assert_response :success
+  test "should login with valid credentials" do
+    post login_path, params: { email: "user1@example.com", password: "password" }
+    assert_redirected_to admin_root_path
+    assert_equal users(:one).id, session[:user_id]
   end
 
-  test "should get destroy" do
-    get sessions_destroy_url
-    assert_response :success
+  test "should not login with invalid credentials" do
+    post login_path, params: { email: "user1@example.com", password: "wrong" }
+    assert_response :unprocessable_entity
+    assert_nil session[:user_id]
+  end
+
+  test "should logout" do
+    # Login first
+    post login_path, params: { email: "user1@example.com", password: "password" }
+    assert session[:user_id].present?
+
+    # Then logout
+    delete logout_path
+    assert_redirected_to root_path
+    assert_nil session[:user_id]
   end
 end
